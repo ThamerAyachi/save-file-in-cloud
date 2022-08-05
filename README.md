35553eb5f1fc1f424f66729d76dcb2e257d1057d
Thamer Ayachi <thamerjak3@gmail.com>
2022-08-05 18:22:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Save in Cloud
+
+## Description
+
+This project for save file in cloud using public api ([api.imgbb](https://api.imgbb.com/)).
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+# watch mode
+$ npm run dev
+# build for production mode
+$ npm run build
+# build typescript watch mode
+$ npm run typescript:dev
+# build scss watch mode
+$ npm run scss:dev
+```
+
+## Stay in touch
+
+- Author - [Thamer Ayachi](https://github.com/ThamerAyachi)
+
+## License
+
+Save in Cloud project is [MIT licensed](LICENSE).
---
+++ src/typescript/index.ts
@@ -0,0 +1,27 @@
+// variables
+const fileInput = document.querySelector("#file");
+const btn = document.querySelector(".btn");
+const axios = (window as any).axios;
+const form = new FormData();
+const show = document.querySelector(".show");
+
+// functions
+const showImg = async (url: string) => {
+  const img: any = document.createElement("img");
+  img.src = url;
+  show?.appendChild(img);
+};
+
+(btn as Element).addEventListener("click", async () => {
+  form.append("image", (fileInput as any).files[0]);
+  try {
+    const res = await axios.post(
+      "https://api.imgbb.com/1/upload?key=e6a735fac9ee98b1897034ee6315d69b",
+      form
+    );
+
+    showImg(res.data.data.display_url);
+  } catch (err) {
+    console.log(err);
+  }
+});
